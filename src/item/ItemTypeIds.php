08b6d9956cf00c7ddedbecccac643cff4f140786
Dylan T. <odigiman@gmail.com>
2024-11-14 18:03:07 +0000

    REEEEEEEE

--- src/item/ItemTypeIds.php
+++ src/item/ItemTypeIds.php
@@ -325,7 +325,7 @@ final class ItemTypeIds{
 	public const PITCHER_POD = 20286;
 	public const NAME_TAG = 20287;
 	public const GOAT_HORN = 20288;
-  public const ENDER_EYE = 20289;
+	public const ENDER_EYE = 20289;
 
 	public const FIRST_UNUSED_ITEM_ID = 20290;
 
